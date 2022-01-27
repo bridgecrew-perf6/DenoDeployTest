542422c04c2b8b8863b960ae971685c19cf0378c
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2022-01-27 10:31:09 +0900

    add console.log

--- handler.ts
+++ handler.ts
@@ -10,29 +10,43 @@ export const handler = (req: Request): Response => {
   }
 
   const { socket: ws, response } = Deno.upgradeWebSocket(req);
+  let id: string;
   ws.onmessage = (e: MessageEvent) => {
     if (e.data === "connect web") {
       const room = new Room(ws);
+      console.log(`room created ${room.id}`);
+      id = room.id;
       rooms.push(room);
       ws.send(room.id);
       return;
     }
     if (e.data.match(/^connect pc/)) {
-      const id: string = e.data.split(" ")[2];
+      id = e.data.split(" ")[2];
       const room = rooms.find((r: Room) => r.id === id);
       if (room) {
         room.pc = ws;
+        console.log(`pc connected ${room.id}`);
       }
       return;
     }
     if (e.data.match(/^change/)) {
       const text: string = e.data.split(" ")[1];
-      const id: string = e.data.split(" ")[2];
       const room = rooms.find((r: Room) => r.id === id);
       if (room) {
         room.web.send(`change ${text}`);
+        console.log(`update sended to ${room.id}: ${text}`);
       }
       return;
+    }
+  };
+
+  ws.onclose = () => {
+    const room = rooms.find(room => room.id === id);
+    if (room) {
+      room.web.close();
+      room.pc.close();
+      rooms.splice(rooms.findIndex(room => room.id === id), 1);
+      console.log(`room closed ${id}`);
     }
   };
 
